bdf8ff19ac708bef92ee82c0db98ac38dd508e31
RoniKurv <144007110+RoniKurv@users.noreply.github.com>
2025-06-09 17:23:10 +0300

    Add files via upload

---
+++ qgis2web_2025_06_09-14_29_27_875757/styles/City_5_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_City_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "15.600000000000001px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 3.0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("wikipedia") !== null) {
+        labelText = String(feature.get("wikipedia"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 9.999999999999996 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
---
+++ qgis2web_2025_06_09-14_29_27_875757/styles/SouthwestFinlandoptimalsolarareas_2_style.js
@@ -0,0 +1,97 @@
+var size = 0;
+var placement = 'point';
+function categories_SouthwestFinlandoptimalsolarareas_2(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case '1-vuot. kuivaheinä-, säilörehu-, tuorerehunurmet':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(122,122,122,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(219,85,194,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Monimuotoisuuspelto, niitty 1. ja 2. vuosi':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(122,122,122,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(136,63,232,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Monimuotoisuuspelto, riista':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(122,122,122,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(139,222,23,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Moniv. kuivaheinä-, säilörehu- ja tuorerehunurmet':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(122,122,122,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(225,164,42,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Pysyvä kuivah.,säilör., tuorer. (väh 5, alle10 v)':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(122,122,122,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(60,209,85,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Tilapäisesti viljelemätön ala':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(122,122,122,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(233,120,127,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Viherkesanto (nurmi ja niitty)':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(122,122,122,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(75,128,233,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(122,122,122,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(115,206,198,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_SouthwestFinlandoptimalsolarareas_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("KASVIKOO_1");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_SouthwestFinlandoptimalsolarareas_2(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
